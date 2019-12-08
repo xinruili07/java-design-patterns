2fc10d3e7fb55713a324985ced50a6276d4ee213
xinruili07 <xinruili@hotmail.com>
2019-12-08 18:55:31 -0500

    Added composite and decorator design pattern demo

---
+++ BridgeDealer/CardImages.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A class to store and manage images of the 52 cards.
+ */
+public final class CardImages 
+{
+	private static final String IMAGE_SUFFIX = ".gif";
+	private static final String[] RANK_CODES = {"2", "3", "4", "5", "6", "7", "8", "9", "t", "j", "q", "k", "a"};
+	private static final String[] SUIT_CODES = {"c", "d", "s", "h"};	
+	
+	private static Map<String, ImageIcon> aCards = new HashMap<String, ImageIcon>();
+	
+	private CardImages()
+	{}
+	
+	/**
+	 * Demo/tester to ensure the card images can correctly 
+	 * be found on the classpath.
+	 * 
+	 * @param pArgs Not used.
+	 */
+	public static void main(String[] pArgs)
+	{
+		JFrame frame = new JFrame();
+		frame.add(new JLabel(CardImages.getBack()));
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	
+	/**
+	 * Return the image of a card.
+	 * @param pCard the target card
+	 * @return An icon representing the chosen card.
+	 */
+	public static ImageIcon getCard( Card pCard )
+	{
+		return getCard( getCode( pCard ) );
+	}
+	
+	/**
+	 * Return an image of the back of a card.
+	 * @return An icon representing the back of a card.
+	 */
+	public static ImageIcon getBack()
+	{
+		return getCard( "b" );
+	}
+	
+	/**
+	 * Return an image of the joker.
+	 * @return An icon representing the joker.
+	 */
+	public static ImageIcon getJoker()
+	{
+		return getCard( "j" );
+	}
+	
+	private static String getCode( Card pCard )
+	{
+		return RANK_CODES[ pCard.getRank().ordinal() ] + SUIT_CODES[ pCard.getSuit().ordinal() ];		
+	}
+	
+	private static ImageIcon getCard( String pCode )
+	{
+		ImageIcon lIcon = (ImageIcon) aCards.get( pCode );
+		if( lIcon == null )
+		{
+			lIcon = new ImageIcon(CardImages.class.getClassLoader().getResource("images/" +  pCode + IMAGE_SUFFIX ));
+			aCards.put( pCode, lIcon );
+		}
+		return lIcon;
+	}
+}
---
+++ BridgeDealer/Deck.java
@@ -0,0 +1,53 @@
+import java.util.Collections;
+import java.util.Stack;
+
+/**
+ * A deck of cards.
+ */
+public class Deck
+{
+	private Stack<Card> aCards;
+	
+	/**
+	 * Creates an empty deck.
+	 */ 
+	public Deck()
+	{
+		aCards = new Stack<Card>();
+	}
+	
+	
+	/**
+	 * Resets the deck with all 52 cards and shuffles it. 
+	 */
+	public void shuffle()
+	{
+		aCards.clear();
+		for( Card.Suit suit : Card.Suit.values() )
+		{
+			for( Card.Rank rank : Card.Rank.values())
+			{
+				aCards.push(new Card(rank, suit));
+			}
+		}
+		Collections.shuffle(aCards);
+	}
+	
+	/**
+	 * @return True if there are no cards in the deck.
+	 */
+	public boolean isEmpty()
+	{
+		return aCards.isEmpty();
+	}
+	
+	/**
+	 * @return The top card in the deck.
+	 * @pre !isEmpty()
+	 */
+	public Card draw()
+	{
+		assert !isEmpty();
+		return aCards.pop();
+	}
+}
